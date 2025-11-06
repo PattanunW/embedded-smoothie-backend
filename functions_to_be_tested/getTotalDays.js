bae06283ed6979ef8c2098984d5c900aeea2c03e
PattanunW <6733191521@student.chula.ac.th>
2025-11-06 15:04:21 +0700

    remove unwanted files 1

--- functions_to_be_tested/getTotalDays.js
+++
@@ -1,5 +0,0 @@
-function getTotalDays(start, end) {
-    const diffMs = new Date(end).getTime() - new Date(start).getTime();
-    return diffMs / (1000 * 60 * 60 * 24) + 1;
-}
-module.exports = { getTotalDays };
